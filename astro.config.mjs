a0699af5c0eb240646bee34f2743898275cf59d4
mandarino43 <nilarroyo43@gmail.com>
2026-02-03 13:18:07 +0100

    styling done

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from "astro-icon"
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
   }
